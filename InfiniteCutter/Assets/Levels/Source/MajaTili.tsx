9741be6a336d2448f29c0e7e1a5320fb519f990e
Rok Kos <rocki5555@gmail.com>
2018-12-01 19:40:32 +0100

    New assets for tiles

--- InfiniteCutter/Assets/Levels/Source/MajaTili.tsx
+++ InfiniteCutter/Assets/Levels/Source/MajaTili.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="texture" tilewidth="128" tileheight="128" tilecount="7" columns="7">
- <image source="../../Atlases/texture.png" width="896" height="128"/>
+<tileset name="texture_2" tilewidth="128" tileheight="128" tilecount="9" columns="9">
+ <image source="../../Atlases/texture_2.png" width="1152" height="128"/>
 </tileset>
