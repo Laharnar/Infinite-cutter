716be813d6adc2779420abc828b91995d3463d6f
Rok Kos <rocki5555@gmail.com>
2018-12-01 18:28:45 +0100

    Unsed stuff

--- InfiniteCutter/Assets/Levels/Source/TiledTest.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="TiledTest" tilewidth="23" tileheight="23" tilecount="18" columns="9">
- <image source="../../Atlases/TiledTest.png" width="208" height="46"/>
-</tileset>
