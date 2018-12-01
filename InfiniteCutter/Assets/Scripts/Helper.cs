bd1395d19a1dc8ad929c2b9df02fbdf6885b9dc4
Rok Kos <rocki5555@gmail.com>
2018-12-02 00:16:46 +0100

    Failed attempt at creating meshes

---
+++ InfiniteCutter/Assets/Scripts/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Helper : MonoBehaviour {
+
+    public static Mesh SpriteToMesh(Sprite sprite) {
+        Mesh mesh = new Mesh();
+        mesh.SetVertices(Array.ConvertAll(sprite.vertices, i => (Vector3)i).ToList());
+        mesh.SetUVs(0, sprite.uv.ToList());
+        mesh.SetTriangles(Array.ConvertAll(sprite.triangles, i => (int)i), 0);
+
+        return mesh;
+    }
+}
---
+++ InfiniteCutter/Assets/Scripts/TilePhysicsObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePhysicsObject : MonoBehaviour {
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] MeshCollider meshCollider;
+    [SerializeField] Sprite sprite;
+	// Use this for initialization
+	void Start () {
+        Mesh meshFromSprite = Helper.SpriteToMesh(sprite);
+        meshFilter.mesh = meshFromSprite;
+        meshCollider.sharedMesh = meshFromSprite;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
