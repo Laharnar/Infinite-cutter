65434b513d1afc84d4387e8768d0cada565199bd
Rok Kos <rocki5555@gmail.com>
2018-12-02 13:32:23 +0100

    Sounds implemented

---
+++ InfiniteCutter/Assets/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField] List<AudioClip> audioClips;
+    [SerializeField] AudioSource audioSource;
+
+    private int CurrentSoundPlaying = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!audioSource.isPlaying) {
+            audioSource.clip = audioClips[CurrentSoundPlaying];
+            audioSource.Play();
+            CurrentSoundPlaying++;
+            CurrentSoundPlaying %= audioClips.Count;
+        }
+	}
+}
