b33cd4d045f85802ca7dc56124e2e3ac7bf5ecbd
Rok Kos <rocki5555@gmail.com>
2018-12-02 14:09:34 +0100

    Background music playing

--- InfiniteCutter/Assets/SoundManager.cs
+++ InfiniteCutter/Assets/SoundManager.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
-
+    [SerializeField] bool FulBackGroundMusic = true;
+    [SerializeField] AudioClip backgroundMusic;
     [SerializeField] List<AudioClip> audioClips;
     [SerializeField] AudioSource audioSource;
-
+    
     private int CurrentSoundPlaying = 0;
-	
-	// Update is called once per frame
-	void Update () {
-        if (!audioSource.isPlaying) {
+
+    private void Start() {
+        if (FulBackGroundMusic) {
+            audioSource.clip = backgroundMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        } else {
+            audioSource.loop = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!FulBackGroundMusic && !audioSource.isPlaying) {
             audioSource.clip = audioClips[CurrentSoundPlaying];
             audioSource.Play();
             CurrentSoundPlaying++;
