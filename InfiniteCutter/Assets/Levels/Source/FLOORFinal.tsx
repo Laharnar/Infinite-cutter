05fe68d32ad1d48d39b7c7a4e087afc6d251f7be
Rok Kos <rocki5555@gmail.com>
2018-12-02 11:09:16 +0100

    New Bottom parts and first level

---
+++ InfiniteCutter/Assets/Levels/Source/FLOORFinal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="FLOORFinal" tilewidth="1536" tileheight="640" tilecount="4" columns="4">
+ <image source="../../Atlases/textureButton.png" width="6144" height="640"/>
+</tileset>
