9b6e374c001dcadf014c486744ce3645fbc32239
Rok Kos <rocki5555@gmail.com>
2018-12-02 10:06:48 +0100

    Camera follow and scene structure

---
+++ InfiniteCutter/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] Transform player;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = player.position;
+        pos.y = transform.position.y;
+        pos.z = transform.position.z;
+
+        pos = Vector3.Lerp(pos, transform.position, 0.5f);
+        transform.position = pos;
+
+    }
+}
